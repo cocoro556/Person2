3ce5a539bf25dc45517c38626cc7c5b57e514b56
cocoro <yuto.nakayama0@gmail.com>
2024-02-29 00:48:19 +0900

    third commit

--- src/Test.java
+++ src/Test.java
@@ -3,12 +3,14 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		// コンストラクタ1を使ってインスタンス化
 		Person taro = new Person();
 		taro.name = "taro";
 		taro.age = 18;
 		System.out.println(taro.name);
 		System.out.println(taro.age);
 
+		// コンストラクタ2を使ってインスタンス化
 		Person jiro = new Person("jiro", 20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
