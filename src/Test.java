8bef48f6e43f606a657f22bf4c300d5b059d0b81
cocoro <yuto.nakayama0@gmail.com>
2024-02-29 00:45:44 +0900

    second commit

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,31 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
+		Person taro = new Person();
+		taro.name = "taro";
+		taro.age = 18;
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+
+		Person jiro = new Person("jiro", 20);
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+
+		// コンストラクタ3を使ってインスタンス化
+		Person saburo = new Person("saburo");
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		// コンストラクタ4を使ってインスタンス化
+		Person unknown = new Person(25);
+		System.out.println(unknown.name);
+		System.out.println(unknown.age);
+
+		// コンストラクタ5を使ってインスタンス化
+		Person hanako = new Person(17, "hanako");
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
 
 	}
 
